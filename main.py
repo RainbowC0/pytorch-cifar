f92e83c0b1cbd219e7c83899c6a5ea39eb95b539
RainbowC0 <67918391+RainbowC0@users.noreply.github.com>
2023-11-25 15:56:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 '''Train CIFAR10 with PyTorch.'''
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch import nn, optim
 import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-
-import torchvision
-import torchvision.transforms as transforms
+from torchvision import transforms, datasets
 
 import os
 import argparse
 
 from models import *
-from utils import progress_bar
+# from utils import progress_bar
 
-
+print(os.getcwd())
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
@@ -22,6 +18,15 @@ parser.add_argument('--resume', '-r', action='store_true',
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+best_epoch = 0
+best_loss = 0
+keep_train_loss = [] # 记录每次训练的loss
+keep_test_loss = [] # 记录每次测试的loss
+keep_train_acc = [] # 记录每次训练的正确率
+keep_test_acc = [] # 记录每次测试的正确率
+
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
@@ -39,12 +44,12 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
+testset = datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
@@ -70,7 +75,9 @@ print('==> Building model..')
 # net = RegNetX_200MF()
 net = SimpleDLA()
 net = net.to(device)
+net_name = 'DLA'
 if device == 'cuda':
+    from torch.backends.cudnn import cudnn
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
@@ -78,15 +85,16 @@ if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    checkpoint = torch.load('./checkpoint/'+net_name+'.pth')
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+# optimizer = optim.SGD(net.parameters(), lr=args.lr,
+#                      momentum=0.9, weight_decay=5e-4)
+optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=5e-4) #added
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50)
 
 
 # Training
@@ -109,12 +117,18 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        print('Loss: %.3f | Acc: %.3f%% (%d/%d)'% (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    else:
+        keep_train_loss.append(train_loss/(batch_idx+1)) #add
+        keep_train_acc.append(100.*correct/total) #add
+        # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        #             % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
 
 def test(epoch):
     global best_acc
+    global best_loss #added
+    global best_epoch #added
     net.eval()
     test_loss = 0
     correct = 0
@@ -130,12 +144,18 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            print('Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                  % (test_loss/(batch_idx+1), 100.*correct/total, correct, total)) #modified
+        else: #added
+            keep_test_loss.append(test_loss/(batch_idx+1)) #added
+            keep_test_acc.append(100*correct/total) #added
 
     # Save checkpoint.
     acc = 100.*correct/total
     if acc > best_acc:
+        best_epoch = epoch #add
+        best_acc = acc #add
+        best_loss = test_loss / (batch_idx + 1) #add
         print('Saving..')
         state = {
             'net': net.state_dict(),
@@ -144,11 +164,31 @@ def test(epoch):
         }
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
-        best_acc = acc
+        torch.save(state, f'./checkpoint/{net_name}.pth')
 
+start_time = time() #added
 
-for epoch in range(start_epoch, start_epoch+200):
+keep_epoch = range(start_epoch, start_epoch+50) # 每次训练的epoch
+for epoch in keep_epoch:
     train(epoch)
     test(epoch)
     scheduler.step()
+
+end_time = time() #added
+
+def save_data():
+    if not os.path.isdir('fig'):
+        os.mkdir('fig')
+    with open(f'./fig/{net_name}_acc.csv', 'w') as f:
+        for i in len(keep_epoch):
+            f.write('%d,%f,%f\n'
+                    % (keep_epoch[i], keep_test_acc[i], keep_train_acc[i]))
+    with open(f'./fig/{net_name}_loss.csv', 'w') as f:
+        for i in len(keep_epoch):
+            f.write('%d,%f,%f\n'
+                    % (keep_epoch[i], keep_test_loss[i], keep_train_loss[i]))
+
+print('runtime : %.3f'%((end_time - start_time)/60)) #add
+print('Test: Loss: %.3f | Best_Acc: %.3f | epoch: %d' % (best_loss, best_acc, best_epoch)) #add
+print('Train: Loss: %.3f | Acc: %.3f' % (keep_train_loss[best_epoch], keep_train_acc[best_epoch]))
+save_data()
